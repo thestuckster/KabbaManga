1d650a168279f3adeae6337ca2d3dcf3c56d93da
thestuckster <sstucky3@gmail.com>
2016-06-19 01:16:06 -0500

    added title images to test list

--- KabbaManga/www/js/controllers/mangaListCtrl.js
+++ KabbaManga/www/js/controllers/mangaListCtrl.js
@@ -1,9 +1,10 @@
 var app = angular.module('kabaMangaApp');
 
 app.controller('mangaListCtrl', ["$scope", "$http", function($scope, $http) {
-
   // allows CORS requests.
   $http.defaults.useXDomain = true;
+
+  //setup scope objects.
   $scope.manga = [];
 
 //get a list of manga
@@ -12,11 +13,14 @@ app.controller('mangaListCtrl', ["$scope", "$http", function($scope, $http) {
     url: "https://www.mangaeden.com/api/list/0/"
   }).then(function success(res) {
 
-    for(var i = 0; i < 50; i++) {
-      var mangaList = res.data["manga"][i];
-      $scope.manga.push(mangaList["t"]);
-    }
-    console.log($scope.manga);
-  });
+    var mangaList = res.data["manga"];
+    for(var i = 0; i < 9; i++) {
 
+      var currentManga = {};
+      currentManga.title = mangaList[i]["t"];
+      currentManga.image = mangaList[i]["im"]
+
+      $scope.manga.push(currentManga);
+    }
+  });
 }]);
