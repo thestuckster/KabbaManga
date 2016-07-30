5bb37d6b49e0348fb62263f05f5fb8486911b822
thestuckster <sstucky3@gmail.com>
2016-07-30 01:27:33 -0500

    image cache dependcy fix

--- KabbaManga/www/js/app.js
+++ KabbaManga/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('kabaMangaApp', ['ionic','kabaMangaApp.controllers'])
+angular.module('kabaMangaApp', ['ionic', 'ionic.ion.imageCacheFactory','kabaMangaApp.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -41,7 +41,7 @@ angular.module('kabaMangaApp', ['ionic','kabaMangaApp.controllers'])
     }
   })
 
-  //TODO: probably not needed. 
+  //TODO: probably not needed.
   .state('app.browse', {
       url: '/browse',
       views: {
